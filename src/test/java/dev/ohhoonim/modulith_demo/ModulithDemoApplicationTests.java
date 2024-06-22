bcc3e52f4e9495ea5a41def92ee60cc64d46cb7b
mathew <donnj95@gmail.com>
2024-06-22 14:50:22 +0900

    init

---
+++ src/test/java/dev/ohhoonim/modulith_demo/ModulithDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ohhoonim.modulith_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulithDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
